9c2d7a0247210d0ee8ab5709374be9fd32fbeacd
maurogigena <maurogigena9@gmail.com>
2024-05-22 20:22:07 -0300

    Home not done

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,29 +1,42 @@
 import React from 'react';
-import { Carousel, Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Carousel, Container, Row, Col, Card, Button, ListGroup } from 'react-bootstrap';
+
+import TecnologiaAd from '../../assets/tecnología-ad.webp';
+import OfertasAd from '../../assets/ofertas-ad.webp';
+import EntregaSeguraAd from '../../assets/entrega-segura-ad.webp';
+
+import Stanley from '../../assets/stanley-card.jpg';
+import Samsung from '../../assets/samsung-card.jpg';
+import TheNorthFace from '../../assets/thenorthface-card.jpg';
+
+import Chromecast4 from '../../assets/chromecast4.jpg';
+import KitStanley from '../../assets/kit-stanley.webp';
+import Airpods from '../../assets/airpods.webp';
+import Notebook from '../../assets/laptop-dell.jpg';
 
 const Home = () => {
   return (
-    <Container>
+    <Container style={{ marginTop:'0px' }}>
       {/* Carousel */}
       <Carousel>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="url_de_tu_imagen1.jpg"
+            src={TecnologiaAd}
             alt="First slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="url_de_tu_imagen2.jpg"
+            src={OfertasAd}
             alt="Second slide"
           />
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="url_de_tu_imagen3.jpg"
+            src={EntregaSeguraAd}
             alt="Third slide"
           />
         </Carousel.Item>
@@ -53,130 +66,262 @@ const Home = () => {
         </Col>
         <Col>
           <Card>
-            <Card.Body>Suscríbete a nuestro newsletter para enterarte primero de nuestras ofertas!</Card.Body>
+            <Card.Body>Suscríbete a nuestro Newsletter! Enterate primero de nuestras ofertas!</Card.Body>
           </Card>
         </Col>
       </Row>
 
       {/* Three Square Cards */}
-      <Row className="mt-4">
-        <Col>
+      <Row className="mt-2">
+      <Col>
           <Card>
-            <Card.Body>
-              <Button variant="primary">Ver productos</Button>
-            </Card.Body>
+            <Card.Img src={Samsung} alt="Third slide" />
+            <Card.ImgOverlay className="d-flex justify-content-start align-items-end p-3">
+              <Button variant="primary" className="mb-3 ml-3">Ver productos</Button>
+            </Card.ImgOverlay>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Body>
-              <Button variant="primary">Ver productos</Button>
-            </Card.Body>
+            <Card.Img src={Stanley} alt="Third slide" />
+            <Card.ImgOverlay className="d-flex justify-content-start align-items-end p-3">
+              <Button variant="primary" className="mb-3 ml-3">Ver productos</Button>
+            </Card.ImgOverlay>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Body>
-              <Button variant="primary">Ver productos</Button>
-            </Card.Body>
+            <Card.Img src={TheNorthFace} alt="Third slide" />
+            <Card.ImgOverlay className="d-flex justify-content-start align-items-end p-3">
+              <Button variant="primary" className="mb-3 ml-3">Ver productos</Button>
+            </Card.ImgOverlay>
           </Card>
         </Col>
       </Row>
 
       {/* Tendencias Section */}
       <h2 className="mt-4">Tendencias</h2>
-      <Row className="mt-3">
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_tendencia_1.jpg" />
+      <Row className="mt-3 justify-content-center">
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={Chromecast4} />
             <Card.Body>
-              <Card.Title>Producto 1</Card.Title>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
           </Card>
         </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_tendencia_2.jpg" />
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={KitStanley} />
             <Card.Body>
-              <Card.Title>Producto 2</Card.Title>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
           </Card>
         </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_tendencia_3.jpg" />
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={Airpods} />
             <Card.Body>
-              <Card.Title>Producto 3</Card.Title>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
           </Card>
         </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_tendencia_4.jpg" />
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={Notebook} />
             <Card.Body>
-              <Card.Title>Producto 4</Card.Title>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
             </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_tendencia_5.jpg" />
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
             <Card.Body>
-              <Card.Title>Producto 5</Card.Title>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
             </Card.Body>
           </Card>
         </Col>
       </Row>
 
-      {/* Los Más Vendidos Section */}
-      <h2 className="mt-4">Los más vendidos</h2>
-      <Row className="mt-3">
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_vendido_1.jpg" />
+      {/* Los Mas Vendidos Section */}
+      <h2 className="mt-4">Los Más Vendidos</h2>
+      <Row className="mt-3 justify-content-center">
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
             <Card.Body>
-              <Card.Title>Producto 1</Card.Title>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
             </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} sm={6} md={4} lg={3} xl={2} className="d-flex justify-content-center">
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up the
+                bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item>Cras justo odio</ListGroup.Item>
+              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+            <Card.Body>
+              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">Another Link</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      {/* Three Square Cards */}
+      <Row className="mt-4">
+      <Col>
+          <Card>
+            <Card.Img src={Samsung} alt="Third slide" />
+            <Card.ImgOverlay className="d-flex justify-content-start align-items-end p-3">
+              <Button variant="primary" className="mb-3 ml-3">Ver productos</Button>
+            </Card.ImgOverlay>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_vendido_2.jpg" />
-            <Card.Body>
-              <Card.Title>Producto 2</Card.Title>
-            </Card.Body>
+            <Card.Img src={Stanley} alt="Third slide" />
+            <Card.ImgOverlay className="d-flex justify-content-start align-items-end p-3">
+              <Button variant="primary" className="mb-3 ml-3">Ver productos</Button>
+            </Card.ImgOverlay>
           </Card>
         </Col>
         <Col>
           <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_vendido_3.jpg" />
-            <Card.Body>
-              <Card.Title>Producto 3</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_vendido_4.jpg" />
-            <Card.Body>
-              <Card.Title>Producto 4</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_vendido_5.jpg" />
-            <Card.Body>
-              <Card.Title>Producto 5</Card.Title>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col>
-          <Card>
-            <Card.Img variant="top" src="url_de_la_imagen_producto_vendido_6.jpg" />
-            <Card.Body>
-              <Card.Title>Producto 6</Card.Title>
-            </Card.Body>
+            <Card.Img src={TheNorthFace} alt="Third slide" />
+            <Card.ImgOverlay className="d-flex justify-content-start align-items-end p-3">
+              <Button variant="primary" className="mb-3 ml-3">Ver productos</Button>
+            </Card.ImgOverlay>
           </Card>
         </Col>
       </Row>
